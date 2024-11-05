3eb76320eb2c9e888fa1de98d9156c474b1aa1e2
kawax <kawaxbiz@gmail.com>
2024-11-05 10:48:33 +0900

    command

--- command/app/Providers/AppServiceProvider.php
+++ command/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Console\LexiconContractsCommand;
+use App\Console\LexiconEnumCommand;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->commands([
+            LexiconEnumCommand::class,
+            LexiconContractsCommand::class,
+        ]);
     }
 }
