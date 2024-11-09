016bba5a44274688fa9efd30b8579fad45c1f624
kawax <kawaxbiz@gmail.com>
2024-11-09 11:15:59 +0900

    Create Union.php

---
+++ src/Attributes/Union.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Revolution\AtProto\Lexicon\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER)]
+class Union
+{
+    public function __construct(public array $refs)
+    {
+    }
+}
