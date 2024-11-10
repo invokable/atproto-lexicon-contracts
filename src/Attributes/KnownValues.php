c874ba4fef9227663f9c251bc02f6eb6ae4c7f9e
kawax <kawaxbiz@gmail.com>
2024-11-10 16:14:50 +0900

    Create KnownValues.php

---
+++ src/Attributes/KnownValues.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Revolution\AtProto\Lexicon\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER)]
+class KnownValues
+{
+    public function __construct(public array $values)
+    {
+    }
+}
