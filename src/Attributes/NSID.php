8c88e4ec388105f9b4797da7cc874072d5d66148
kawax <kawaxbiz@gmail.com>
2024-11-11 12:21:42 +0900

    Update NSID.php

--- src/Attributes/NSID.php
+++ src/Attributes/NSID.php
@@ -4,7 +4,7 @@ namespace Revolution\AtProto\Lexicon\Attributes;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_METHOD)]
+#[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_CLASS)]
 class NSID
 {
     public function __construct(public string $id)
