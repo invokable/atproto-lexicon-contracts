0e19a7abc3dcf0237a5ae030edfe5a7f62f08af3
kawax <kawaxbiz@gmail.com>
2024-11-11 10:15:03 +0900

    Create AbstractUnion.php

---
+++ src/Types/AbstractUnion.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Revolution\AtProto\Lexicon\Types;
+
+/**
+ * Union object
+ *
+ * ```
+ * [
+ *     '$type' => '...',
+ *     '...' => [],
+ * ]
+ * ```
+ */
+abstract class AbstractUnion
+{
+    protected string $type;
+}
