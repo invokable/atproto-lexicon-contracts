48e912c5a8b36fff28241306030fd7f959034840
kawax <kawaxbiz@gmail.com>
2024-11-11 10:15:02 +0900

    Create AbstractBlob.php

---
+++ src/Types/AbstractBlob.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Revolution\AtProto\Lexicon\Types;
+
+/**
+ * Blob object
+ *
+ * ```
+ * [
+ *     '$type' => 'blob',
+ *     'ref' => [
+ *         '$link' => '...',
+ *     'mimeType' => 'image/png',
+ *     'size' => 100000,
+ * ]
+ * ```
+ */
+abstract class AbstractBlob
+{
+    protected string $type = 'blob';
+
+    protected string $link;
+
+    protected string $mimeType;
+
+    protected int $size;
+}
