5d8440b99759377559d0957646e2206e3b5702d8
kawax <kawaxbiz@gmail.com>
2024-11-13 17:23:26 +0900

    Update lexicons.php

--- example/lexicons.php
+++ example/lexicons.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Revolution\AtProto\Lexicon\Lexicons\AbstractLexicons;
 
-$lex = new class extends AbstractLexicons {};
+$lex = new class extends AbstractLexicons {
+};
 
 var_dump($lex->get('app.bsky.actor.defs'));
