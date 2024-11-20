86e767aadee19120c3583a299e01527f2fc0327b
kawax <kawaxbiz@gmail.com>
2024-11-20 12:24:10 +0900

    Update LexiconRecordCommand.php

--- command/app/Console/Commands/LexiconRecordCommand.php
+++ command/app/Console/Commands/LexiconRecordCommand.php
@@ -237,7 +237,7 @@ class LexiconRecordCommand extends Command
                 }
 
                 if (filled($deprecated)) {
-                    $deprecated = "    #[Deprecated]";
+                    $deprecated = '    #[Deprecated]';
                 }
 
                 return collect($properties)
