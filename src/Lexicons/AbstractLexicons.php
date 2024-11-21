197eab8fa7326edfbf5681193510a7ebb27d62dd
kawax <kawaxbiz@gmail.com>
2024-11-21 22:20:00 +0900

    Update AbstractLexicons.php

--- src/Lexicons/AbstractLexicons.php
+++ src/Lexicons/AbstractLexicons.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Revolution\AtProto\Lexicon\Lexicons;
 
+use InvalidArgumentException;
+
 abstract class AbstractLexicons
 {
     protected array $defs;
@@ -13,9 +15,35 @@ abstract class AbstractLexicons
         $this->defs = require_once __DIR__.'/defs.php';
     }
 
-    public function get(string $id): array
+    /**
+     * This is just an example, so inherit this class and create your own methods.
+     *
+     * @param  string  $id  e.g. `app.bsky.feed.post`, `app.bsky.feed.defs#postView`, `com.atproto.repo.createRecord#main`
+     */
+    public function getDef(string $id): array
     {
-        return $this->defs[$id] ?? [];
+        if (str_contains($id, '#')) {
+            [$id, $hash] = explode('#', $id, 2);
+        }
+
+        $def = $this->defs[$id] ?? [];
+
+        if (! empty($hash)) {
+            $def = $def['defs'][$hash] ?? [];
+        }
+
+        return $def;
+    }
+
+    public function getDefOrThrow(string $id): array
+    {
+        $def = $this->getDef($id);
+
+        if (empty($def)) {
+            throw new InvalidArgumentException();
+        }
+
+        return $def;
     }
 
     public function all(): array
