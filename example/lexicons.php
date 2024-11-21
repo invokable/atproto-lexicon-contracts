61f245c154c3d3826bb04b3653da1a5af6ae0b4d
kawax <kawaxbiz@gmail.com>
2024-11-21 22:20:04 +0900

    Update lexicons.php

--- example/lexicons.php
+++ example/lexicons.php
@@ -7,4 +7,12 @@ use Revolution\AtProto\Lexicon\Lexicons\AbstractLexicons;
 $lex = new class extends AbstractLexicons {
 };
 
-var_dump($lex->get('app.bsky.actor.defs'));
+var_dump($lex->getDef('app.bsky.feed.post'));
+var_dump($lex->getDef('app.bsky.embed.external#external'));
+var_dump($lex->getDef('app.bsky.feed.post#main'));
+
+try {
+    $def = $lex->getDefOrThrow('app.bsky.feed._throw');
+} catch (InvalidArgumentException $e) {
+    var_dump($e::class);
+}
