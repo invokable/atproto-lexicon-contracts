191281f1a460e1fbd6f5c08ca645f70613efa80d
kawax <kawaxbiz@gmail.com>
2024-12-08 21:53:17 +0900

    Create Output.php

---
+++ src/Attributes/Output.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Revolution\AtProto\Lexicon\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+class Output
+{
+    public function __construct(public array $schema)
+    {
+    }
+}
