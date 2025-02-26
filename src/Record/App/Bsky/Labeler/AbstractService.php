d7cbb21cc525dee911aa29a6159499c2ea643442
kawax <1502086+kawax@users.noreply.github.com>
2025-02-26 03:39:04 +0000

    lexicon update

--- src/Record/App/Bsky/Labeler/AbstractService.php
+++ src/Record/App/Bsky/Labeler/AbstractService.php
@@ -28,4 +28,22 @@ abstract class AbstractService
 
     #[Format('datetime')]
     protected string $createdAt;
+
+    /**
+     * The set of report reason 'codes' which are in-scope for this service to review and action. These usually align to policy categories. If not defined (distinct from empty array), all reason types are allowed.
+     */
+    #[Ref('com.atproto.moderation.defs#reasonType')]
+    protected ?array $reasonTypes = null;
+
+    /**
+     * The set of subject types (account, record, etc) this service accepts reports on.
+     */
+    #[Ref('com.atproto.moderation.defs#subjectType')]
+    protected ?array $subjectTypes = null;
+
+    /**
+     * Set of record types (collection NSIDs) which can be reported to this service. If not defined (distinct from empty array), default is any record type.
+     */
+    #[Format('nsid')]
+    protected ?array $subjectCollections = null;
 }
